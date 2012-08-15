f2a0bad930008fa8af34c49923440df4f75f9344
Rene Schweiger <rschweiger@gmx.de>
2012-08-15 08:31:38 +0200

    erstes comit. Leeres Rails-Projekt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dreiwsechs::Application.config.secret_token = '211765e2a2f8612092adfdd3b42b9cef17be069ed0da71f605f9aa3b9e346c9ac964ec0a4865c920eb24bf352f2d18cc840835717af835081f9906695102e307'
